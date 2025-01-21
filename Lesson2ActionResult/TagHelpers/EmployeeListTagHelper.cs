323a6aa2c857358d2e845998bf75fe7299e17c5f
Shirinov Nurlan Anar <shirinov_n@itstep.edu.az>
2025-01-21 09:37:46 +0400

    custom tag helper

---
+++ Lesson2ActionResult/TagHelpers/EmployeeListTagHelper.cs
@@ -0,0 +1,76 @@
+﻿using Lesson2ActionResult.Entites;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lesson2ActionResult.TagHelpers
+{
+   
+    [HtmlTargetElement("employee-list")]
+    public class EmployeeListTagHelper : TagHelper
+    {
+        public List<Employee> _employees;
+
+        public EmployeeListTagHelper()
+        {
+            _employees = new List<Employee>()
+            {
+                  new Employee
+                {
+                    Id = 1,
+                    CityId = 1,
+                    Firstname="Aqshin",
+                    Lastname = "Ahmedli"
+                },
+
+                new Employee
+                {
+                    Id = 2,
+                    CityId = 2,
+                    Firstname="Nizami",
+                    Lastname = "Amirov"
+                },
+                new Employee
+                {
+                    Id = 3,
+                    CityId = 2,
+                    Firstname="Ali",
+                    Lastname = "Aliyev"
+                },
+                       new Employee
+                {
+                    Id = 1,
+                    CityId = 1,
+                    Firstname="Fazil",
+                    Lastname = "Ahmedli"
+                },
+
+                new Employee
+                {
+                    Id = 2,
+                    CityId = 2,
+                    Firstname="Kamal",
+                    Lastname = "Amirov"
+                }
+            };
+        }
+        private const string ListCountAttribute = "count";
+
+        [HtmlAttributeName(ListCountAttribute)]
+        public int ListCount { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "section";
+            var query = _employees.Take(ListCount);
+            StringBuilder sb = new StringBuilder();
+            foreach (var item in query)
+            {
+                sb.AppendFormat("<h2><a href='/employee/detil/{0}'>{1}</a></h2>" , item.Id , item.Firstname);
+            }
+
+            output.Content.SetHtmlContent(sb.ToString());
+        }
+    }
+}
